eeb1aa78deb81dc2ac7188dd2f652ab8b9712e6c
Wonjong Park <66448572+wpark20@users.noreply.github.com>
2021-09-01 14:20:47 -0400

    initial php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'bb701c0928e49e20e6ecd75fc61616992e5ca8bc',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'bb701c0928e49e20e6ecd75fc61616992e5ca8bc',
+            'dev_requirement' => false,
+        ),
+    ),
+);
