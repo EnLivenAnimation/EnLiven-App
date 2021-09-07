41e90e2805e7f72bc945e73124a99e6a53344561
Wonjong Park <66448572+wpark20@users.noreply.github.com>
2021-09-07 14:15:18 -0400

    laravel

--- public/includes/dbh.inc.php
+++ public/includes/dbh.inc.php
@@ -1,11 +1,12 @@
 <?php
 
+$host = "0.0.0.0";
 $serverName = "remotemysql.com";
 $dBUsername = "1dCoH3Y2yF";
 $dBPassword = "N4fjbSMGqH";
 $dBName = "1dCoH3Y2yF";
 
-$conn = mysqli_connect($serverName,$dBUsername,$dBPassword,$dBName);
+$conn = mysqli_connect($host, $serverName,$dBUsername,$dBPassword,$dBName);
 if(!$conn){
     die("Connection failed: " . mysqli_connect_error());
 }
