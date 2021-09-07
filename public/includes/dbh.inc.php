56705e9dfc26f0632f45bb1634bc8e1cfafbb0c7
Wonjong Park <66448572+wpark20@users.noreply.github.com>
2021-09-07 14:17:46 -0400

    laravel fix 2

--- public/includes/dbh.inc.php
+++ public/includes/dbh.inc.php
@@ -5,8 +5,9 @@ $serverName = "remotemysql.com";
 $dBUsername = "1dCoH3Y2yF";
 $dBPassword = "N4fjbSMGqH";
 $dBName = "1dCoH3Y2yF";
+$port = "3306";
 
-$conn = mysqli_connect($host, $serverName,$dBUsername,$dBPassword,$dBName);
+$conn = mysqli_connect($host, $serverName,$dBUsername,$dBPassword,$dBName,$port);
 if(!$conn){
     die("Connection failed: " . mysqli_connect_error());
 }
