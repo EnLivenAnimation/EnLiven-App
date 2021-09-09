0070c3078c415417839e6f4b63e86f0b2618c91e
Wonjong Park <66448572+wpark20@users.noreply.github.com>
2021-09-09 14:33:19 -0400

    push index

--- public/index.php
+++ public/index.php
@@ -2,146 +2,182 @@
     session_start();
 ?>
 
+<?php
+
+
+if($_POST["message"]) {
+    mail("enlivenwon@gmail.com", "User Feedback",
+    $_POST["insert your message here"]. "From: pqqgood@gmail.com");
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>EnLiven Animate</title>
-  <link rel="shortcut icon" type="image/jpg" href="res/icons/favicon.ico"/>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style/index.css">
+        <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href='https://fonts.googleapis.com/css?family=Courier Prime' rel='stylesheet'>
+    </head>
+    <script>window.onbeforeunload = function () {window.scrollTo(0, 0);}</script>
+    <body>
+        <nav id="navbar">
+            <script src = "scripts/mainpage.js"></script>
+            <a></a>
+            <br>
+            <button href= "index.php" id="b1">Animate</button>
+            <button id="b2" >Resources</button>
+            <button id="b3">Community</button>
+            <?php
+                    if(isset($_SESSION["useruid"])){
+                        echo "<button onclick=\"location.href='includes/logout.inc.php'\"; id='b4'>Log Out</button>";
+                    }
+                    else{
+                        echo "<button onclick=\"location.href='login.php'\";  id='b4'>Log In</button>";
+                    }
+            ?>
+        </nav>
 
-  <!-- STYLESHEET -->
-  <link rel="stylesheet" href="style/index.css">
-  <!-- EXTERNAL SCRIPTS -->
-  <!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> -->
-  <script src="https://kit.fontawesome.com/3d9aefd90b.js" crossorigin="anonymous"></script>
-  <script src="scripts/pixi/pixi.min.js"></script>
-  <!-- FRONTEND SCRIPTS -->
-  <script src="scripts/navbar.js" async></script>
-  <script src="scripts/home-navbar.js" async></script>
-  <script src="scripts/properties.js" async></script>
-  <script src="scripts/treeview.js" async></script>
-  <script src="scripts/import.js" async></script>
-  <!-- BACKEND SCRIPTS -->
-  <script src="scripts/display.js" async></script>
-  <script src="scripts/buttons.js" async></script>
-  <script src="scripts/moving.js" async></script>
-  <script src="scripts/scaling.js" async></script>
-  <script src="scripts/rotating.js" async></script>
-  <script src="scripts/converting.js" async></script>
-  <script src="scripts/timeline.js" async></script>
-  <script src="scripts/playing.js" async></script>
-  <script src="scripts/looping.js" async></script>
-  <script src="scripts/input.js" async></script>
-  <script src="scripts/add.js" async></script>
-  <script src="scripts/export.js" async></script>
-  <script src="scripts/onion.js" async></script>
-  <script src="scripts/undo.js" async></script>
-  <script src="scripts/background.js" async></script>
-  <script src="scripts/keyboard.js" async></script>
-  <script src="scripts/Parenting.js" async></script>
-  <!-- MAIN SCRIPT -->
-  <script src="scripts/main.js" async></script>
-</head>
-
-<body>
-  <div id="grid">
-    <!-- TOP NAVBAR -->
-    <div id="navbar">
-      <a id="home" class="navbar-button" href="newhome.html"><i class="fas fa-leaf"></i></a>
-      <div class="dropdown">
-        <button onclick="displayDropdown('file-dropdown')" class="navbar-button">File</button>
-        <div id="file-dropdown" class="dropdown-content">
-          <a href="javascript:location.reload(true)">New File</a>
-          <button onclick="exportE()">Export</button>
+        <div id="hero">
+            <div class="hero-text">
+                <br>
+                <div>
+                    <hero style="font-size: 140px; text-align: center;" id="hero1">EnLiven</hero>
+                    <hero2 style="font-size:30px; text-align: center; color: teal;" id ="hero2">Bring your creations to life</hero2>
+                </div>
+                <br>
+            </div>
         </div>
-      </div>
-      <div class="dropdown">
-        <button onclick="displayDropdown('edit-dropdown')" class="navbar-button">Edit</button>
-        <div id="edit-dropdown" class="dropdown-content">
-          <button onclick="undo();">Undo</button>
-          <!-- <a href="#redo">Redo</a> -->
-          <!-- <a href="#cut">Cut</a>
-          <a href="#copy">Copy</a>
-          <a href="#paste">Paste</a> -->
+
+        <div id="intro">
+            <div class="description">
+                <br>
+                <h1>01</h1>
+                <h2>EASY</h2>
+                <p>EnLiven has an user interface that is extremely user-friendly and tutorials to guide people. </p>
+                <br>
+            </div>
+            <div class="description">
+                <br>
+                <h1>02</h1>
+                <h2>CONVENIENT</h2>
+                <p>EnLiven is and will always be a free website that is accessible to anyone.</p>
+                <br>
+                <button class='intro-button'>Get Started!</button>    
+            </div>
+            <div class="description">
+                <br>
+                <h1>03</h1>
+                <h2>FREE</h2>
+                <p>EnLiven is and will always be a free website that is accessible to anyone. </p>
+                <br>
+            </div>
         </div>
-      </div>
-      <div class="dropdown">
-        <button onclick="displayDropdown('view-dropdown')" class="navbar-button">View</button>
-        <div id="view-dropdown" class="dropdown-content">
-          <!-- <a href="#fullscreen">Full Screen</a> -->
-          <button onclick="play();">Play</button>
-          <button onclick="playFromStart();">Play from Start</button>
-          <button onclick="pause();">Pause</button>
+
+        <div id="intro2">
+            <script src = "scripts/intro.js"></script>
+            <h1>About Us</h1>
+            <div>
+                <button class="tablink" onclick="openAbout('Introduction', this, '#6B9775')" id="defaultOpen">Introduction</button>
+                <button class="tablink" onclick="openAbout('Wonjong Park', this, '#9BC8A5')">Wonjong Park</button>
+                <button class="tablink" onclick="openAbout('Edward Wu', this, '#7AD17E')">Edward Wu</button>
+                <button class="tablink" onclick="openAbout('Justin Zou', this, '#75B884')">Justin Zou</button>
+            </div>
+            <div id="intro2-main">
+                <div id="Introduction" style ="background-color:#6B9775; font-size: 20px; text-align: center;" class="tabcontent">
+                <h1>Introduction</h1>
+                <br>
+                <p>EnLiven was first thought of due to our interest in animation. However, a quick Google search later, our group of 4 realized that there were no animation tutorial sites. Despite animation becoming a bigger and bigger field than ever before, it is still a daunting art to get into. So we created EnLiven. Meant to convey the idea of bringing your creations to life, EnLiven will allow  users to learn the 12 basic principles of animation using our interactive tutorials and simplified animation engine. Eventually we hope to create a community where users can interact and give tips to each other. Ultimately, users will be able to move on to more advanced animation engines with EnLiven as a starting point. </p>
+                </div>
+
+                <div id="Wonjong Park" style ="background-color:#9BC8A5" class="tabcontent">
+                <h1>Wonjong Park</h1>
+                <div class="introduction-person">
+                    <div style="background-image: url('wonjong-intro.jpg');" class="introduction-img"></div>
+                    <div class="introduction-text">
+                        <p>Age: 18</p>
+                        <p>School: Stuyvesant High School</p>
+                        <p>Interested subjects: Biology, Physics,Math, CS</p>
+                        <p>Hobbies: Animation, Webtoon, 3D Printing, Jag-saw puzzle, Gundam</p>
+                        <p>Least favorite person in team: Justin</p>
+                    </div>
+                </div>
+                </div>
+
+                <div id="Edward Wu" style ="background-color:#7AD17E" class="tabcontent">
+                <h1>Edward Wu</h1>
+                <div class="introduction-person">
+                    <div class="introduction-img-ed"></div>
+                    <div class="introduction-text">
+                        <p>Age: 16</p>
+                        <p>School: Stuyvesant High School</p>
+                        <p>Interested subjects: Math, CS, Chem, Physics, Stat</p>
+                        <p>Hobbies: Town of Salem, Algodoo marble race, Korean</p>
+                        <p>Least favorite person in team: Justin</p>
+                    </div>
+                </div>
+                </div>
+
+                <div id="Justin Zou" style ="background-color:#75B884" class="tabcontent">
+                <h1>Justin Zou</h1>
+                <div class="introduction-person">
+                    <div style="background-image: url('justin-intro.jpeg');" class="introduction-img"></div>
+                    <div class="introduction-text">
+                        <p>Age: 17</p>
+                        <p>School: Stuyvesant High School</p>
+                        <p>Interested subjects:CS</p>
+                        <p>Hobbies: Video Games, Manga</p>
+                        <p>Least favorite person in team: Wonjong</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <script>document.getElementById("defaultOpen").click();</script>
         </div>
-      </div>
-      <div class="dropdown">
-        <button onclick="displayDropdown('add-dropdown')" class="navbar-button">Add</button>
-        <div id="add-dropdown" class="dropdown-content">
-          <button onclick="addCircle();">Circle</button>
-          <button onclick="addSquare();">Square</button>
-          <button onclick="addTriangle();">Triangle</button>
-          <button onclick="addHuman();">Human</button>
+        
+        <div class="footer-dark">
+            <footer>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6 col-md-3 item">
+                            <h3>Features</h3>
+                            <ul>
+                                <li><a href="#">Animate</a></li>
+                                <li><a href="#">12 Principles</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-6 col-md-3 item">
+                            <h3>Members</h3>
+                            <ul>
+                                <li><a href="#">Wonjong Park</a></li>
+                                <li><a href="#">Edward Wu</a></li>
+                                <li><a href="#">Justin Zou</a></li>
+                                <li><a href="#">Eric Wang(Former)</a></li>
+                                <li><a href="#">Victor Jiao(Mentor)</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-6 item text">
+                            <div class="email-form">
+                                <form action="includes/feedback_form.php" method="post">
+                                    <input class="email1" type="text" id="Name" name="Name" placeholder="Name">
+                                    <input class="email1" type="email" id="Email" name="Email" placeholder="Email Address">
+                                    <textarea placeholder="Your feedback" class ="email2" id="Message" name="Message" cols="50" rows="10" ></textarea>
+                                    <input class = "email-button" type="submit" value="&#8594;">
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
+                    <p class="copyright">EnLiven © 2021</p>
+                </div>
+            </footer>
         </div>
-      </div>
 
-      <button id="delete" class="navbar-button" type="button" onclick="deleteSelectedSprite();">Delete</button>
-      <div class="dropdown">
+    </body>
 
-      <button onclick="displayDropdown('background-dropdown')" class="navbar-button">Background</button>
-      <div id="background-dropdown" class="dropdown-content">
-        <button onclick="addDefault();">Default</button>
-        <button onclick="addBlackBackground();">BlackBackground</button>
-        <button onclick="addYourName();">YourNameBackground</button>
-      </div>
-
-    </div>
-    </div>
-    
-    <!-- RELATIONS PANEL -->
-    <div id="relations" class="panel">
-      <ul id="relations-list">
-      </ul>
-    </div>
-
-    <!-- CANVAS PANEL -->
-    <div id="canvas">
-    </div>
-
-    <!-- PROPERTIES PANEL -->
-    <div id="properties" class="panel">
-      <p id="x">X</p><input type="number" id="xpos" name="xpos" min="-100" max="100000">
-      <p id="y">Y</p><input type="number" id="ypos" name="ypos" min="-100" max="100000">
-      <p id="w">W</p><input type="number" id="width" name="width" min="0" max="100000">
-      <p id="h">H</p><input type="number" id="height" name="height" min="0" max="100000">
-      <p id="r">R</p><input type="number" id="rotation" name="rotation" min="-100000" max="100000">
-      <p id = "c">Color Picker<input type="color" id="colorpicker" value="#1b9834">
-      <button onclick="changeColor()" class="navbar-button"> Change Color</button></p>
-    </div>
-    
-    <!-- TIMELINE PANEL -->
-    <div id="timeline" class="panel">
-      <button class="timeline-button" onclick="play();"><i class="fas fa-play"></i></button>
-      <button class="timeline-button" onclick="pause();"><i class="fas fa-pause"></i></button>
-      <button class="timeline-button" onclick="addKeyframe();"><i class="fas fa-plus"></i></button>
-      <button id="deleteKeyframeButton" class="delete-disabled" onclick=""><i class="fas fa-minus"></i></button>
-      <button class="timeline-button" onclick="editKeyframe();">REPLACE</button>
-      <span></span>
-      <button id="onion" class="onion-disabled" type="button" onclick="toggleOnionSkins();"><i class="fas fa-ellipsis-h"></i></button>
-      <button id="loop" class="loop-disabled" type="button" onclick="toggleLooping();"><i class="fas fa-undo-alt"></i></button>
-      <div id="fpsSlider-container" style="background-color: #d1e1d1; border-radius: 10px; padding: 10px;">
-        <p style="text-align: justify;">FPS</p>
-        <label id="fpsSlider-label" for="slider">FPS</label>
-        <input id="fpsSlider" type="range" name="fpsSlider" min="20" max="120" value="60">
-      </div>
-      <div id="tweensSlider-container"style="background-color: #d1e1d1; border-radius: 10px; padding: 10px;" >
-        <p style="text-align: justify;">TWEENS</p>
-        <label id="tweensSlider-label" for="slider">TWEENS</label>
-        <input id="tweensSlider" type="range" name="tweensSlider" min="0" max="30" value="20">
-      </div>
-      <div id="timeline-keyframes"></div>
-    </div>
-  </div>
-</body>
 </html>
